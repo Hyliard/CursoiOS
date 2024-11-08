4cc99e4306471b8f532ac6f96b9773d5c7bda088
hyliard <luisgerardomartinezh@gmail.com>
2024-11-08 15:40:31 -0300

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Luis Martinez Hernandez on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
